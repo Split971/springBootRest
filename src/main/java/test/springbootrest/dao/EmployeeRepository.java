357b8c9ba7fee9c9852a587bbb8c68d90a3a5e14
Dmitriy <split995@mail.ru>
2020-01-01 16:01:04 +0300

    create springBootRest

---
+++ src/main/java/test/springbootrest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package test.springbootrest.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import test.springbootrest.entity.Employee;
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
